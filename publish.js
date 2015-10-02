16271f02cc7f6e6b7ee6868a80028fcf9ac5ebb0
vinngn1 <vinh@jelli.com>
2015-10-02 15:55:11 -0700

    fix double class navBar

--- publish.js
+++ publish.js
@@ -306,58 +306,6 @@ function buildNav(members) {
         });
     }
 
-    if (members.events.length) {
-        _.each(members.events, function (v) {
-            nav.push({
-                type: 'event',
-                longname: v.longname,
-                name: v.name,
-                members: find({
-                    kind: 'member',
-                    memberof: v.longname
-                }),
-                methods: find({
-                    kind: 'function',
-                    memberof: v.longname
-                }),
-                typedefs: find({
-                    kind: 'typedef',
-                    memberof: v.longname
-                }),
-                events: find({
-                    kind: 'event',
-                    memberof: v.longname
-                })
-            });
-        });
-    }
-
-    if (members.classes.length) {
-        _.each(members.classes, function (v) {
-            nav.push({
-                type: 'class',
-                longname: v.longname,
-                name: v.name,
-                members: find({
-                    kind: 'member',
-                    memberof: v.longname
-                }),
-                methods: find({
-                    kind: 'function',
-                    memberof: v.longname
-                }),
-                typedefs: find({
-                    kind: 'typedef',
-                    memberof: v.longname
-                }),
-                events: find({
-                    kind: 'event',
-                    memberof: v.longname
-                })
-            });
-        });
-    }
-
     if (members.classes.length) {
         _.each(members.classes, function (v) {
             nav.push({
